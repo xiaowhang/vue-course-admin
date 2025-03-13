b4cb506819ea4c16698556a66de27055ac054c43
xiaowhang <b13528723800@163.com>
2025-03-13 14:57:30 +0800

    ✨ feat(resource category): 实现资源类别的创建和编辑弹窗
    优化资源类别管理弹窗的用户界面和交互逻辑：
    - 将表单标题改为动态显示创建/编辑状态
    - 简化表单字段，仅保留类别名称和排序
    - 添加表单重置功能
    - 将界面文案本地化为中文
    - 调整标签宽度为更合理的80px
    
    改进了用户体验并符合中文使用习惯

--- src/views/resource-category/DialogCreateEdit.vue
+++ src/views/resource-category/DialogCreateEdit.vue
@@ -1,46 +1,46 @@
 <template>
-  <el-dialog v-model="dialogFormVisible" title="Shipping address" width="500">
-    <el-form :model="form">
-      <el-form-item label="Promotion name" :label-width="formLabelWidth">
+  <el-dialog v-model="dialogFormVisible" :title="msgText + '资源类别'" width="500">
+    <el-form :model="form" ref="formRef">
+      <el-form-item label="类别名称" :label-width="formLabelWidth" prop="name">
         <el-input v-model="form.name" autocomplete="off" />
       </el-form-item>
-      <el-form-item label="Zones" :label-width="formLabelWidth">
-        <el-select v-model="form.region" placeholder="Please select a zone">
-          <el-option label="Zone No.1" value="shanghai" />
-          <el-option label="Zone No.2" value="beijing" />
-        </el-select>
+      <el-form-item label="排序" :label-width="formLabelWidth" prop="sort">
+        <el-input v-model="form.sort" autocomplete="off" />
       </el-form-item>
     </el-form>
     <template #footer>
       <div class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="dialogFormVisible = false"> Confirm </el-button>
+        <el-button @click="close">取消</el-button>
+        <el-button type="primary" @click="close"> 提交 </el-button>
       </div>
     </template>
   </el-dialog>
 </template>
 
 <script setup lang="ts">
+import type { FormInstance } from 'element-plus'
+
 const dialogFormVisible = ref(false)
-const formLabelWidth = '140px'
+const formLabelWidth = '80px'
 
 const form = reactive({
   name: '',
-  region: '',
-  date1: '',
-  date2: '',
-  delivery: false,
-  type: [],
-  resource: '',
-  desc: '',
+  sort: 0,
 })
 
-const handleCreate = () => {
-  dialogFormVisible.value = true
+const formRef = ref<FormInstance>()
+
+const close = () => {
+  dialogFormVisible.value = false
+  formRef.value?.resetFields()
 }
 
 const msgText = ref('创建')
 
+const handleCreate = () => {
+  dialogFormVisible.value = true
+  msgText.value = '创建'
+}
 const handleEdit = (id: number) => {
   handleCreate()
   msgText.value = '编辑'
